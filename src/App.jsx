11eaab387fc4996a27f4273485ae3a8f6540752d
Misha <vellsevul@mail.ru>
2024-07-31 23:57:43 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import Collapse from './components/Collapse';
+
+function App() {
+  return (
+    <div>
+      <Collapse>
+        <p>
+          Это не те дроиды, которых вы ищите.
+        </p>
+      </Collapse>
+    </div>
+  );
+}
+
+export default App;
